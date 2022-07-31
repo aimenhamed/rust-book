5abf986bfa9d44cb7e1ec2a6823fcdef2022a587
Aimen Hamed <aimenhamed@live.com.au>
2022-07-31 22:04:04 +1000

    chapter 1&2 completed

---
+++ chapter-1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/aimen/code/rust-book/chapter-1/hello_cargo/target/debug/hello_cargo: /Users/aimen/code/rust-book/chapter-1/hello_cargo/src/main.rs
---
+++ chapter-1/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/aimen/code/rust-book/chapter-1/hello_cargo/target/release/hello_cargo: /Users/aimen/code/rust-book/chapter-1/hello_cargo/src/main.rs
